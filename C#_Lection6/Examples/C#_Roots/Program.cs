a3cc4810cbc9da4c3c6ab877bfadc0e6c8d16860
Alexander R <sashta.home@gmail.com>
2023-02-08 02:44:54 +0300

    add Lection6

---
+++ C#_Lection6/Examples/C#_Roots/Program.cs
@@ -0,0 +1,20 @@
+﻿Roots Solve(double a, double b, double c)
+{
+  var d = b * b - 4 * a * c;
+  double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+  double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+  return new Roots { X1 = x1, X2 = x2 };
+}
+
+Console.WriteLine();
+double a = 4;
+double b = -12;
+double c = 5;
+Roots roots = Solve(a, b, c);
+Console.WriteLine($"Для квадратного уравнения вида y = {a} * x^2 + {b} * x + {c} корнями являются x1 = {roots.X1} и x2 = {roots.X2}.");
+
+class Roots
+{
+  public double X1 { get; set; }
+  public double X2 { get; set; }
+}
---
+++ C#_Lection6/Examples/C#_StringParsing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine();
+Console.WriteLine(text);
+Console.WriteLine();
+
+var data = text.Split(" ") // Можно указать тип string[], но в общем виде лучше оставить var.
+               .Select(item => item.Split(','))
+               .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1]))) // Т.н. лямбды - функционал в C#.
+               .Where(e => e.x % 2 == 0)
+               .Select(point => (point.x * 10, point.y + 10))
+               .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+  Console.WriteLine(data[i]);
+  // for (int k = 0; k < data[i].Length; k++)
+  // {
+  //   Console.WriteLine(data[i][k]);
+  // }
+}
